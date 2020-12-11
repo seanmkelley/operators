62419b55ffba31bde5e2a9630199738d9e576f1d
seankelley <smkelley.1997@gmail.com>
2020-12-11 02:39:44 -0500

    commit tree 588bf744a13528cfb20d79c240e2a705f2265a47

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,10 @@
 def unsafe?(speed)
     if speed > 60
-        true
+        return true
     elsif speed < 40
-        true
+        return true
     else
-        false
+        return false
     end
 end
 
